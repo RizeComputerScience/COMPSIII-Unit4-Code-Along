1d2217206f11b8ff19cdc373e0bfd7e144f497ad
stevenjlance <steven.j.lance@gmail.com>
2025-03-10 11:15:19 -0400

    added solutions

--- app.py
+++ app.py
@@ -31,7 +31,30 @@ class ShoppingCart:
 
 # Create ClothingProduct and ElectronicsProduct classes here
 class ClothingProduct(Product):
-    pass
+    # Call the `__init__` method in the child class using either `Product`. Set the `name`, `price`, and `sku` properties.
+    def __init__(self, name, price, sku, size, color):
+        Product.__init__(self, name, price, sku)
+        # Set the `size` and `color` to the values that are passed into the constructor.
+        self.size = size
+        self.color = color
+    # Create a `clothing_info` method that returns information about the object.
+    def clothing_info(self):
+        return f"{self.name} (SKU: {self.sku}) - ${self.price:.2f} - Size: {self.size}, Color: {self.color}"
 
 class ElectronicsProduct(Product):
-    pass
+    # Call the `__init__` method in the child class using `super()` to show you can do either! Set the `name`, `price`, and `sku` properties.
+    def __init__(self, name, price, sku, warranty_months):
+        super().__init__(name, price, sku)
+        # Set the `warranty_months` to the value that are passed into the constructor. 
+        self.warranty_months = warranty_months
+    
+    # Create a method called `electronics_info` that takes the object as an argument. It should return a string.
+    def electronics_info(self):
+        return f"{self.name} (SKU: {self.sku}) - ${self.price:.2f} - Warranty: {self.warranty_months} months"
+    
+    # Create a method called `under_warranty` that takes the object and `months` as an argument. If the number of months passed in is less than the `warranty_months`, then return `True`. Otherwise, return `False`.
+    def under_warranty(self, months):
+        if self.warranty_months > months:
+            return True
+        else:
+            return False
